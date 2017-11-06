ab8e35d131c849c625a5171642430e289471ed0b
Tim Holt <tim@astronautweb.co>
2017-11-06 20:58:37 +1000

    reset

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-snapshot';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<App />, div);
-});
