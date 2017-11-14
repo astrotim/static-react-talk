1e8dfe1993e064f191e1a93aa544aa530f4520e8
Tim Holt <tim.holt@abc.net.au>
2017-11-14 12:16:57 +1000

    reset

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-import Home from './Home';
-import Post from './Post';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <header className="App-header">
-            <Link to="/">
-              <img src={logo} className="App-logo" alt="logo" />
-            </Link>
-            <h1 className="App-title">Static React</h1>
-          </header>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/post/:id" component={Post} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
