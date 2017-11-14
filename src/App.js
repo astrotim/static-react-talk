f6c533ee8648c254cfcf84477fd5292ae8f5a6a6
Tim Holt <tim.holt@abc.net.au>
2017-11-14 13:19:44 +1000

    add some pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Post from './Post';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <Link to="/">Home</Link> <Link to="/post/">Post</Link>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/post" component={Post} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
