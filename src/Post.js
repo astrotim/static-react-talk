7d938b31e8bc818eae298359419e8c8d3e89bd4a
Tim Holt <tim.holt@abc.net.au>
2017-11-14 11:46:29 +1000

    added CMS content

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { createClient } from 'contentful';
 
 class Post extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+
+  componentWillMount() {
+    const client = createClient({
+      space: process.env.REACT_APP_SPACE_ID,
+      accessToken: process.env.REACT_APP_ACCESS_TOKEN
+    });
+
+    client
+      .getEntry(this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          data: response.fields
+        });
+      })
+      .catch(console.error);
+  }
+
   render() {
-    return <p>Post</p>;
+    if (!this.state.data) return null;
+
+    return (
+      <div>
+        <h1>{this.state.data.title}</h1>
+        {this.state.data.content}
+      </div>
+    );
   }
 }
 
